08d8d51525c0b7d5e183c6a1c3aa6e0adfc9c61f
Luke Tainton <Luke.Tainton@mail.bcu.ac.uk>
2017-02-20 13:21:24 +0000

    Create game start script

---
+++ main.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+
+red = 18
+amber = 23
+green = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(red,GPIO.OUT) # RED LIGHT
+GPIO.setup(amber,GPIO.OUT) # AMBER LIGHT
+GPIO.setup(green,GPIO.OUT) # GREEN LIGHT
+
+start = raw_input("Would you like to start? (y/n): ")
+if start == y:
+
+    GPIO.output(red,GPIO.HIGH)
+    time.sleep(5)
+
+    GPIO.output(amber,GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(red,GPIO.LOW)
+    GPIO.output(amber,GPIO.LOW)
+    GPIO.output(green,GPIO.HIGH)

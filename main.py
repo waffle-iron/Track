68a81dd664d78c4b44d0cf1b49857bc65801d721
Luke Tainton <Luke.Tainton@mail.bcu.ac.uk>
2017-02-20 17:32:15 +0000

    Added comments so you know what stuff does!

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
+# IMPORT MODULES
 import RPi.GPIO as GPIO
 import time
 
-
+# SET VARIABLES
 red = 24
 amber = 23
 green = 8
 track = 18
 
+# INITIALISE RPi.GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# SETUP GPIO PINS
 GPIO.setup(red,GPIO.OUT) # RED LIGHT
 GPIO.setup(amber,GPIO.OUT) # AMBER LIGHT
 GPIO.setup(green,GPIO.OUT) # GREEN LIGHT
 GPIO.setup(track,GPIO.OUT) # TRACK POWER
 pwm = GPIO.PWM(track, 500) # SET UP PWM MANAGEMENT
 
+# RED LIGHT
 GPIO.output(red,GPIO.HIGH)
 GPIO.output(amber,GPIO.LOW)
 GPIO.output(green,GPIO.LOW)
 GPIO.output(track,GPIO.LOW)
 time.sleep(3)
 
+# RED AND AMBER LIGHTS
 GPIO.output(red,GPIO.HIGH)
 GPIO.output(amber,GPIO.HIGH)
 GPIO.output(green,GPIO.LOW)
 GPIO.output(track,GPIO.LOW)
 time.sleep(2)
 
+# GREEN LIGHT AND TRACK POWER
 GPIO.output(red,GPIO.LOW)
 GPIO.output(amber,GPIO.LOW)
 GPIO.output(green,GPIO.HIGH)

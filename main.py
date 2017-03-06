23d4a12a71251e092b0acce1d519682d92b64d1f
Luke Tainton <Luke Tainton>
2017-03-06 11:48:32 +0000

    Changed back to digital output

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ GPIO.setup(red,GPIO.OUT) # RED LIGHT
 GPIO.setup(amber,GPIO.OUT) # AMBER LIGHT
 GPIO.setup(green,GPIO.OUT) # GREEN LIGHT
 GPIO.setup(track,GPIO.OUT) # TRACK POWER
-pwm = GPIO.PWM(track, 500) # SET UP PWM MANAGEMENT
 
 # RED LIGHT
+print "RED!"
 GPIO.output(red,GPIO.HIGH)
 GPIO.output(amber,GPIO.LOW)
 GPIO.output(green,GPIO.LOW)
@@ -27,6 +27,7 @@ GPIO.output(track,GPIO.LOW)
 time.sleep(3)
 
 # RED AND AMBER LIGHTS
+print "AMBER!"
 GPIO.output(red,GPIO.HIGH)
 GPIO.output(amber,GPIO.HIGH)
 GPIO.output(green,GPIO.LOW)
@@ -34,7 +35,16 @@ GPIO.output(track,GPIO.LOW)
 time.sleep(2)
 
 # GREEN LIGHT AND TRACK POWER
+print "GREEN!"
 GPIO.output(red,GPIO.LOW)
 GPIO.output(amber,GPIO.LOW)
 GPIO.output(green,GPIO.HIGH)
-pwm.start(100)
+GPIO.output(track,GPIO.HIGH)
+time.sleep(5)
+
+# CLEAN UP
+print "Script ending, cleaning up GPIO pins. Please wait."
+GPIO.cleanup()
+
+# EXIT
+print "Script ended."
